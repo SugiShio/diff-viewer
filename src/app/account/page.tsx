f9e13b116ddbf57b1eb19e505c89b10131c0fc32
Shoko Sugito <fruitmachine0@gmail.com>
2025-02-26 21:48:31 +0900

    ざっくりレイアウト

---
+++ src/app/account/page.tsx
@@ -0,0 +1,8 @@
+export const Index = () => {
+  return (
+    <div>
+      <h1>Account Page</h1>
+    </div>
+  );
+};
+export default Index;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Header } from "~/components/Header";
+import { Footer } from "~/components/Footer";
+import "~/stylesheets/globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,9 +14,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+    <html lang="ja">
+      <body>
+        <Header />
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
