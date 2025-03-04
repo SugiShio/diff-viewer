0878c7ed18e89ff7abcd02162ca2866c38d6f382
Shoko Sugito <fruitmachine0@gmail.com>
2025-03-04 22:59:09 +0900

    サンプルテキスト

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,13 @@ import "./style.scss";
 import { useState } from "react";
 import { Button } from "~/components/Button";
 import { DiffViewer } from "~/components/DiffViewer";
-
+const SAMPLE_BEFORE =
+  "あのイーハトーヴォのすきとおった風、\n夏でも底に冷たさをもつ青いそら、\nうつくしい森で飾られたモリーオ市、\n郊外のぎらぎらひかる草の波。\nまたそのなかでいっしょになったたくさんのひとたち、\nファゼーロとロザーロ、羊飼のミーロや、\n顔の赤いこどもたち、地主のテーモ、\n山猫博士のボーガント・デストゥパーゴなど、\nいまこの暗い巨きな石の建物のなかで考えていると、\nみんなむかし風のなつかしい青い幻燈のように思われます。";
+const SAMPLE_AFTER =
+  "あのイ一ハトーヴォのすきとおった風、\n夏でも底に冷たさをもつ青いそら、\n美しい森で飾られたモリーオ市、\n郊外のギラギラひかる草の波。\nまたそのなかでいっしょになったたくさんのひとたち、\nファゼーロとロザーロ, 羊飼のミーロや、\n顔の紅い子供たち、地主のテーモ、\n山猫博士のボーガント・デストゥパーゴなど、\nいまこの暗い巨きな石の建物のなかで考えていると、\nみんなむかし風のナツカシイ青い幻燈のように思われます。";
 export default function Home() {
-  const [beforeText, setBeforeText] = useState("");
-  const [afterText, setAfterText] = useState("");
+  const [beforeText, setBeforeText] = useState(SAMPLE_BEFORE);
+  const [afterText, setAfterText] = useState(SAMPLE_AFTER);
   const handleBeforeText = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setBeforeText(e.target.value);
   };
@@ -17,8 +20,8 @@ export default function Home() {
   return (
     <div className="p-index">
       <div className="p-index__textarea-wrapper">
-        <textarea className="p-index__textarea" name="" id="" onChange={handleBeforeText}></textarea>
-        <textarea className="p-index__textarea" name="" id="" onChange={handleAfterText}></textarea>
+        <textarea className="p-index__textarea" name="" id="" onChange={handleBeforeText} value={SAMPLE_BEFORE} />
+        <textarea className="p-index__textarea" name="" id="" onChange={handleAfterText} value={SAMPLE_AFTER} />
       </div>
       <div className="p-index__button-wrapper">
         <Button text="差分を確認"></Button>
